7db7fac526560bbdcecc3e57c0a818c6cf41cf3a
kidnaped <lishayko@gmail.com>
2022-07-10 22:36:11 +0300

    Add files via upload

---
+++ main_2.py
@@ -0,0 +1,5 @@
+name = input("Who are you?: ")
+drinks = int(input("How many bottles of SHMURDYAK are you capable of drinking? "))
+
+print("You really are a "+name)
+print("Ebat ti alkash, " + str(drinks) + " butilok zaebashit!")
