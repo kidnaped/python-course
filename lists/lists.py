ecafeae575372f2517b5bde012ee6e9e0d2bc6c5
kidnaped <lishayko@gmail.com>
2022-07-15 12:08:21 +0300

    Create lists.py

---
+++ lists/lists.py
@@ -0,0 +1,15 @@
+# list = used t ostore multiple items in a single var
+
+food = ["pizza", "hamburger", "hotdog", "spaghetti"]
+
+food[0] = "sushi"
+
+#food.append("govno sobaki")    # adds an item in the end of a list
+#food.remove("hotdog")          # removes specific str item from list
+#food.pop()                     # removes specific item under certain index in a list
+#food.insert(0, "kakaha")       # insert an item in specific index of a list
+#food.sort()                    # sorts list in ascending () or descending (reverse = True) order
+#food.clear()                    # clears the list
+
+for i in food:
+    print(i)
