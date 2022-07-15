083278e8100b84cffb556f2c6798984bdc518a23
kidnaped <lishayko@gmail.com>
2022-07-15 12:07:21 +0300

    Delete main_2.py

--- main_2.py
+++
@@ -1,5 +0,0 @@
-name = input("Who are you?: ")
-drinks = int(input("How many bottles of SHMURDYAK are you capable of drinking? "))
-
-print("You really are a "+name)
-print("Ebat ti alkash, " + str(drinks) + " butilok zaebashit!")
